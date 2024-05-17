eb4c386901f9f2f3c28e4356a77ac091288e3203
Ilaheheyd <145252399+Ilaheheyd@users.noreply.github.com>
2024-05-17 14:12:55 +0400

    Add files via upload

---
+++ filmsWebSite/src/App.js
@@ -0,0 +1,39 @@
+import './App.css';
+import Basket from './Components/Basket';
+
+import Index from './Components/Index';
+
+import { useEffect } from 'react';
+
+import { addBasket } from './store/basket';
+
+import { useDispatch } from 'react-redux';
+
+import { Route, Routes } from 'react-router-dom';
+
+import ReadMore from './Components/ReadMore';
+
+function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    fetch(`https://www.omdbapi.com/?s=godfather&apikey=1ef7ec0e`)
+      .then(res => res.json())
+      .then(data => {
+        dispatch(addBasket(data))
+      })
+      .catch(error => console.error(error));
+  }, []);
+
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="movies/" element={<Index />} />
+        <Route path="basket" element={<Basket />} />
+        <Route path="/readmore/:id" element={<ReadMore />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
